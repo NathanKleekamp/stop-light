c38163366d1449b4d362671b5123e82d2844ea90
Nathan Kleekamp <nkleekamp@gmail.com>
2020-09-12 07:27:56 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { Machine } from "xstate";
+import { useMachine } from "@xstate/react";
 import './App.css';
 
+const lightMachine = new Machine({
+  id: "light",
+  initial: "red",
+  states: {
+    green: {
+      on: {
+        TIMER: "red",
+      }
+    },
+    yellow: {
+      on: {
+        TIMER: "green",
+      },
+    },
+    red: {
+      on: {
+        TIMER: "yellow",
+      },
+    },
+  },
+});
+
+const Light = props => {
+  const { color } = props;
+  const styles = {
+    backgroundColor: color,
+    height: "200px",
+    marginBottom: "5px",
+    width: "200px",
+  };
+  return (
+    <div style={styles} />
+  );
+};
+
 function App() {
+  const [current, send] = useMachine(lightMachine);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      send("TIMER");
+      console.log("interval")
+    }, 3000);
+    return () => clearInterval(intervalId)
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Light color={current.matches("red") && "red"} />
+        <Light color={current.matches("yellow") && "yellow"} />
+        <Light color={current.matches("green") && "green"} />
     </div>
   );
 }
